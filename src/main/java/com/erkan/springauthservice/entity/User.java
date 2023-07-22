6f09b93e4079c40fd6441ba6624ec441879a60bd
Erkan <90839872+ErkanCicek@users.noreply.github.com>
2023-07-22 04:42:57 +0200

    added @transactional (#2)

--- src/main/java/com/erkan/springauthservice/entity/User.java
+++ src/main/java/com/erkan/springauthservice/entity/User.java
@@ -1,5 +1,6 @@
 package com.erkan.springauthservice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +17,10 @@ public class User{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long uid;
 
+    @Column(unique = true)
     private String username;
+
+    @Column(unique = true)
     private String email;
     private String password;
     private String role;    
